6cccbdd514c26f1d02f8428687f70877a9649a85
christian-edwards <christianallane@gmail.com>
2021-12-27 12:17:52 -0500

    v3

--- DiceRoller.py
+++ DiceRoller.py
@@ -1,6 +1,27 @@
 import random
 
 
+def rollerWith(sides):
+    try:
+        if sides < 2:
+            raise ValueError
+    except ValueError:
+        print("ERROR Invalid entry: Must be an integer greater than 1.")
+        return
+    randomNumber = random.randrange(1, sides+1)
+    if sides == 2 and randomNumber == 1:
+        print("You flip a coin.. and it lands on.. HEADS")
+    elif sides == 2 and randomNumber == 2:
+        print("You flip a coin.. and it lands on.. TAILS")
+    else:
+        print("You roll a %d sided die... and it lands on %d!" % (sides, randomNumber))
+        if randomNumber == 1:
+            print("Critical failure!")
+        elif randomNumber == sides:
+            print("Critical success!")
+    return randomNumber
+
+
 def roller():
     try:
         sides = int(input("How many sides would you like your die to have?"))
@@ -23,9 +44,10 @@ def roller():
     return randomNumber
 
 
+
 finished = False
 while not finished:
-    roller()
+    rollerWith(6)
     answer = input("Continue? Y/N").upper()
     if answer == "N":
         finished = True
