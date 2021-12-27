b4d9cf74819d2bf8d810b8edff39f3c6bdae0135
christian-edwards <christianallane@gmail.com>
2021-12-27 14:29:07 -0500

    v5.2pep8

--- DiceRoller.py
+++ DiceRoller.py
@@ -1,7 +1,7 @@
 import random
 import logging
 
-#Configuring log
+# Configuring log
 logging.basicConfig(
     filename='mylog.log',
     filemode='w',
@@ -12,28 +12,28 @@ logging.basicConfig(
 logging.info("Program runs, logging information here")
 
 
-def rollerWith(sides):
+def rollerwith(sides):
     try:
         if sides < 2:
             raise ValueError
     except ValueError:
         print("ERROR Invalid entry: Must be an integer greater than 1.")
         return
-    randomNumber = random.randrange(1, sides+1)
+    randomnumber = random.randrange(1, sides+1)
     # If sides are two, it does a coin toss as the dice roll
-    if sides == 2 and randomNumber == 1:
+    if sides == 2 and randomnumber == 1:
         print("You flip a coin.. and it lands on.. HEADS")
-    elif sides == 2 and randomNumber == 2:
+    elif sides == 2 and randomnumber == 2:
         print("You flip a coin.. and it lands on.. TAILS")
     else:
-        print("You roll a %d sided die... and it lands on %d!" % (sides, randomNumber))
+        print("You roll a %d sided die... and it lands on %d!" % (sides, randomnumber))
         # Checks if dice rolls on lowest possible value, does not mean the program is failing
-        if randomNumber == 1:
+        if randomnumber == 1:
             print("Critical failure!")
         # Checks if dice rolls on the highest possible value
-        elif randomNumber == sides:
+        elif randomnumber == sides:
             print("Critical success!")
-    return randomNumber
+    return randomnumber
 
 
 def roller():
@@ -46,33 +46,33 @@ def roller():
         # Logs an error message if the user enters a number less than 2
         logging.error("Invalid Entry: Must be an integer greater than 1")
         return
-    randomNumber = random.randrange(1, sides+1)
-    if sides == 2 and randomNumber == 1:    # If sides are two, it does a coin toss as the dice roll
+    randomnumber = random.randrange(1, sides+1)
+    if sides == 2 and randomnumber == 1:    # If sides are two, it does a coin toss as the dice roll
         # Logs an info message if the user flips a coin and its result
         logging.info("Successful coin flip: HEADS")
         print("You flip a coin.. and it lands on.. HEADS")
-    elif sides == 2 and randomNumber == 2:
+    elif sides == 2 and randomnumber == 2:
         # Logs an info message if the user flips a coin and its result
         logging.info("Successful coin flip: TAILS")
         print("You flip a coin.. and it lands on.. TAILS")
     else:
         # Logs an info message if the user rolls a n sided die and its result
-        logging.info("Successful roll of a %d sided die and lands on: %d" % (sides,randomNumber))
-        print("You roll a %d sided die... and it lands on %d!" % (sides, randomNumber))
-        if randomNumber == 1: # Checks if dice rolls on lowest possible value, does not mean the program is failing
+        logging.info("Successful roll of a %d sided die and lands on: %d" % (sides,randomnumber))
+        print("You roll a %d sided die... and it lands on %d!" % (sides, randomnumber))
+        if randomnumber == 1:  # Checks if dice rolls on lowest possible value, does not mean the program is failing
             print("Critical failure!")
-        elif randomNumber == sides: # Checks if dice rolls on the highest possible value
+        elif randomnumber == sides:  # Checks if dice rolls on the highest possible value
             print("Critical success!")
-    return randomNumber
+    return randomnumber
 
 
 # Takes user back to start of program if they wish to continue, or ends the program if finished
-finished = False
-while not finished:
+FINISHED = False
+while not FINISHED:
     roller()
     answer = input("Continue? Y/N").upper()
     if answer == "N":
-        finished = True
+        FINISHED = True
 
 # Logs a message telling you that the program exited successfully
 logging.info("Program ends, end of logging")
