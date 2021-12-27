22fdeda0bda3eae29e2e87e339f60f5b515e5921
lhnguyen387 <54646063+lhnguyen387@users.noreply.github.com>
2021-12-27 12:04:06 -0600

    v3 + added some comments

--- DiceRoller.py
+++ DiceRoller.py
@@ -9,14 +9,17 @@ def rollerWith(sides):
         print("ERROR Invalid entry: Must be an integer greater than 1.")
         return
     randomNumber = random.randrange(1, sides+1)
+    #If sides are two, it does a coin toss as the dice roll
     if sides == 2 and randomNumber == 1:
         print("You flip a coin.. and it lands on.. HEADS")
     elif sides == 2 and randomNumber == 2:
         print("You flip a coin.. and it lands on.. TAILS")
     else:
         print("You roll a %d sided die... and it lands on %d!" % (sides, randomNumber))
+        #Checks if dice rolls on lowest possible value, does not mean the program is failing
         if randomNumber == 1:
             print("Critical failure!")
+        #Checks if dice rolls on the highest possible value
         elif randomNumber == sides:
             print("Critical success!")
     return randomNumber
@@ -44,7 +47,7 @@ def roller():
     return randomNumber
 
 
-
+#Takes user back to start of program if they wish to continue, or ends the program if finished
 finished = False
 while not finished:
     rollerWith(6)
